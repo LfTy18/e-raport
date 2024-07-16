d74f3f9abd37949e09f8f6ad7f67d93e906616b5
LfTy18 <robbytrianda102@gmail.com>
2024-07-16 17:27:24 +0700

    adding the front-end

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 const siswaRoutes = require('./routes/siswaRoutes');
 const guruRoutes = require('./routes/guruRoutes');
@@ -10,11 +11,12 @@ const nilaiRoutes = require('./routes/nilaiRoutes');
 const absensiRoutes = require('./routes/absensiRoutes');
 
 const app = express();
+dotenv.config();
 
 // Init Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
-
+app.use(cors());
 // Connect to MongoDB
 connectDB();
 
@@ -25,6 +27,8 @@ app.use('/api/mapel', mapelRoutes);
 app.use('/api/kelas', kelasRoutes);
 app.use('/api/nilai', nilaiRoutes);
 app.use('/api/absensi', absensiRoutes);
+
+app.use(express.static('public'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Serverstarted on port ${PORT}`));
